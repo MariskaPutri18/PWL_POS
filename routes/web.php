6a3f83326ec2996aaeb36bbb91192bb077ab6009
MariskaPutri18 <mariskaputri960@gmail.com>
2025-03-06 13:44:02 +0700

    modifikasi tabel user

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\LevelControllerController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
@@ -10,4 +11,5 @@ Route::get('/', function(){
 });
 Route::get('/level',[LevelController::class, 'index']);
 Route::get('/kategori',[KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
