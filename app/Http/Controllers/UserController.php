fc09387782d042855caa3f27552efa41154ab868
MariskaPutri18 <mariskaputri960@gmail.com>
2025-03-12 14:26:55 +0700

    atribut changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,29 +28,20 @@ class UserController extends Controller
      {
         $user = UserModel::create(
             [
-                'username' => 'manager55',
-                'nama' => 'Manager55',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
-        $user-> username = 'manager56';
-        
-        $user->isDirty(); //true
-        $user->isDirty('username'); //true
-        $user->isDirty('nama'); //fales
-        $user->isDirty(['nama', 'username']); //true
-
-        $user->isClean(); //fales
-        $user->isClean('username'); //fales
-        $user->isClean('nama'); //true
-        $user->isClean(['nama', 'username']); //fales
-
+        $user-> username = 'manager12';
         $user->save();
 
-        $user->isDirty(); //fales
-        $user->isClean();//true
-        dd($user->isDirty());
+        $user->wasChanged(); ///true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['nama', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
 
     }
 }
